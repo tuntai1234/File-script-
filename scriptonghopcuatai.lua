1ea29cc3762161efbf7ed3a8a6fda86fe5f37a93
tuntai1234 <phamthihop1904@gmail.com>
2026-06-03 21:04:36 +0700

    Rename GUI from 'GUI của Tài' to 'GUI Tổng hợp'

--- scriptonghopcuatai.lua
+++ scriptonghopcuatai.lua
@@ -1,7 +1,7 @@
 local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
 
 local Window = Rayfield:CreateWindow({
-    Name = "GUI của Tài",
+    Name = "GUI Tổng hợp",
     LoadingTitle = "Đang tải...",
     LoadingSubtitle = "By Tài",
     ConfigurationSaving = {
